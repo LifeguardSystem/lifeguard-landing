3415f036f2bb4dd103387528bb50ad4d9ab8e71c
devneto <miguel.neto320@gmail.com>
2023-09-21 23:13:31 -0300

    feat: create examples

---
+++ src/examples/index.ts
@@ -0,0 +1,4 @@
+export * from './basic-auth';
+export * from './info';
+export * from './plugins';
+export * from './validation';
---
+++ src/examples/plugins.ts
@@ -0,0 +1,11 @@
+export const pluginExample = `import lifeguard_mongodb
+    
+PLUGINS = [
+    lifeguard_mongodb,
+    # other plugins
+]
+
+# You can execute code in the lifeguard startup process
+def setup(_lifeguard_context):
+    pass`
+
